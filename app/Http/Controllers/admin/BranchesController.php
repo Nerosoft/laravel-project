85c1b5cd0b98c7fd0172690e0a7fdf5525963044
unknown <ggamals7744@yahoo.com>
2025-05-12 18:32:42 +0300

    edit code

--- app/Http/Controllers/admin/BranchesController.php
+++ app/Http/Controllers/admin/BranchesController.php
@@ -24,7 +24,9 @@ class BranchesController extends Controller implements LangObject
             case 'branch_delete':
                 return new AppModel('delete', (request()->session()->get('userLogout') === request()->session()->get('userId')?$myDb:Rays::find(request()->session()->get('userId')))[(request()->session()->get('userLogout') === request()->session()->get('userId')?$myDb:Rays::find(request()->session()->get('userId')))['Setting']['Language']]['Error'], 'Branch', (request()->session()->get('userLogout') === request()->session()->get('userId')||request()->input('id')===request()->session()->get('userId')?$myDb:Rays::find(request()->session()->get('userId')))[(request()->session()->get('userLogout') === request()->session()->get('userId')||request()->input('id')===request()->session()->get('userId')?$myDb:Rays::find(request()->session()->get('userId')))['Setting']['Language']]['Message']['BranchesDelete'], isset($myDb['Branch'])?array_keys($myDb['Branch']):array());
             case'change':
-                return new AppModel('delete', $myDb[$myDb['Setting']['Language']]['Error'], 'Branch', $myDb[$myDb['Setting']['Language']]['Message']['BranchesChange'], isset($myDb['Branch'])?array(request()->session()->get('userLogout')=>['Name'=>$myDb[$myDb['Setting']['Language']]['AppSettingAdmin']['BranchMain']]):(isset(Rays::find(request()->session()->get('userLogout'))['Branch'])?Rays::find(request()->session()->get('userLogout'))['Branch']:array()));
+                return new AppModel('delete', $myDb[$myDb['Setting']['Language']]['Error'], 'Branch', $myDb[$myDb['Setting']['Language']]['Message']['BranchesChange'],
+                isset($myDb['Branch'])?array(request()->session()->get('userLogout')=>['Name'=>$myDb[$myDb['Setting']['Language']]['AppSettingAdmin']['BranchMain']]):
+                (isset(Rays::find(request()->session()->get('userLogout'))['Branch'])?Rays::find(request()->session()->get('userLogout'))['Branch']:array(request()->session()->get('userLogout')=>['Name'=>$myDb[$myDb['Setting']['Language']]['AppSettingAdmin']['BranchMain']])));
         }
     }
     public function index(){
