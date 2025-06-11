2f999596c6e0324fa2091225aecf0f1d0db0e851
unknown <ggamals7744@yahoo.com>
2025-06-11 21:20:20 +0300

    edit code

--- app/language/share/EmailPassInformaion.php
+++ app/language/share/EmailPassInformaion.php
@@ -1,9 +1,17 @@
 <?php
 namespace App\language\share;
 use App\MyLanguage;
-class EmailPassInformaion extends ModelLanguage{
+class EmailPassInformaion extends InitPage{
     protected function __construct($help, $reqEmail, $invEmail, $reqPass, $invPass, $labelUserEmail, $labelUserPassword, $hintUserEmail, $hintUserPassword, $settingLanguage, $changeLang, $saveLanguage, $loginUser, $myRadios, $raysId, $language, $title, $direction){
-        parent::__construct($settingLanguage, $changeLang, $saveLanguage, $loginUser, $myRadios, $raysId, $language, $title, $direction);
+        parent::__construct($language, $title, $direction);
+        $this->button1 = $changeLang;
+        $this->button2 = $saveLanguage;
+        $this->button3 = $loginUser;
+        $this->label4 = $settingLanguage;
+        $this->myRadios = array();
+        foreach ($myRadios as $key => $value)
+            $this->myRadios[$key] = new MyLanguage($value);
+        $this->RaysId = $raysId;
         $this->error1 = $invEmail;
         $this->error2 = $reqEmail;
         $this->error3 = $invPass;
