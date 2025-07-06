1c4bb88e935f2b5da786ce4701f718e377a13db0
unknown <ggamals7744@yahoo.com>
2025-07-06 16:32:37 +0300

    edit code

--- app/Http/Controllers/DeleteController.php
+++ app/Http/Controllers/DeleteController.php
@@ -5,31 +5,100 @@ use Illuminate\Validation\Rule;
 class DeleteController extends Controller
 {
     public function __construct(){
-        $ob = Rays::find(request()->session()->get('userId'));
-        request()->validate([
-        'id' => ['required', Rule::in( request()->route('id') === 'Branch'&&isset(Rays::find(request()->session()->get('userLogout'))['Branch'])? array_keys(Rays::find(request()->session()->get('userLogout'))['Branch']) :(isset($ob[request()->route('id')])?array_keys($ob[request()->route('id')]):array()))],
-        ], [
-            'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')][
-                request()->route('id') === 'CurrentOffers'?'CurrentOffersIdRequired':(request()->route('id') === 'Test'?'TestIdRequired':(request()->route('id') === 'Packages'?'PackagesIdRequired':(request()->route('id') === 'Cultures'?'CulturesIdRequired':(request()->route('id') === 'Contracts'?'ContractIdRequired':(request()->route('id') === 'Knows'?'KnowsIdRequired':(request()->route('id') === 'Patent'?'PatentIdRequired':(request()->route('id') === 'Receipt'?'PatientRegisterationIdRequired':'BranchRaysId')))))))],
-            'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')][
-                request()->route('id') === 'CurrentOffers'?'CurrentOffersIdInvalid':(request()->route('id') === 'Test'?'TestIdInvalid':(request()->route('id') === 'Packages'?'PackagesIdInvalid':(request()->route('id') === 'Cultures'?'CulturesIdInvalid':(request()->route('id') === 'Contracts'?'ContractIdInvalid':(request()->route('id') === 'Knows'?'KnowsIdInvalid':(request()->route('id') === 'Patent'?'PatentIdInvalid':(request()->route('id') === 'Receipt'?'PatientRegisterationIdInvalid':'BranchRaysLenght')))))))],
-        ]);
-        if(request()->input('id') === request()->session()->get('userId')&& request()->route('id') === 'Branch'){
+        if(request()->input('id') === request()->session()->get('userId') && request()->route('id') === 'Branch'){
             $ob = Rays::find(request()->session()->get('userLogout'));
+            request()->validate(['id' => ['required', Rule::in(isset($ob[request()->route('id')]) ? array_keys($ob[request()->route('id')]) : array())],
+            ], [
+                'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')]['BranchRaysId'],
+                'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')]['BranchRaysLenght'],
+            ]);
             $this->getDeleteDatabade($ob, request()->route('id'));
             request()->session()->put('userId', request()->session()->get('userLogout'));
             Rays::find(request()->input('id'))->delete();
             $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')]['BranchesDelete'];
-        }
-        else if(request()->route('id') === 'Branch'){
-            $this->getDeleteDatabade(Rays::find(request()->session()->get('userLogout')), request()->route('id'));
+        }else if(request()->route('id') === 'Branch'){
+            $ob = Rays::find(request()->session()->get('userLogout'));
+            request()->validate(['id' => ['required', Rule::in(isset($ob[request()->route('id')]) ? array_keys($ob[request()->route('id')]) : array())],
+            ], [
+                'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')]['BranchRaysId'],
+                'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')]['BranchRaysLenght'],
+            ]);
+            $this->getDeleteDatabade($ob, request()->route('id'));
             Rays::find(request()->input('id'))->delete();
             $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')]['BranchesDelete'];
-        }
-        else{
+        }else if(request()->route('id') === 'CurrentOffers'){
+            $ob = Rays::find(request()->session()->get('userId'));
+            request()->validate(['id' => ['required', Rule::in(isset($ob[request()->route('id')]) ? array_keys($ob[request()->route('id')]) : array())],
+            ], [
+                'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')]['CurrentOffersIdRequired'],
+                'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')]['CurrentOffersIdInvalid'],
+            ]);
             $this->getDeleteDatabade($ob, request()->route('id'));
-            $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')][
-            request()->route('id') === 'CurrentOffers'?'CurrentOffersDelete':(request()->route('id') === 'Test'?'TestDelete':(request()->route('id') === 'Packages'?'PackagesDelete':(request()->route('id') === 'Cultures'?'CulturesDelete':(request()->route('id') === 'Contracts'?'ContractsDelete':(request()->route('id') === 'Knows'?'KnowsDelete':(request()->route('id') === 'Patent'?'PatientsDelete':'PatientRegisterationDelete'))))))];
+            $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')]['CurrentOffersDelete'];
+        }else if(request()->route('id') === 'Test'){
+            $ob = Rays::find(request()->session()->get('userId'));
+            request()->validate(['id' => ['required', Rule::in(isset($ob[request()->route('id')]) ? array_keys($ob[request()->route('id')]) : array())],
+            ], [
+                'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')]['TestIdRequired'],
+                'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')]['TestIdInvalid'],
+            ]);
+            $this->getDeleteDatabade($ob, request()->route('id'));
+            $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')]['TestDelete'];
+        }else if(request()->route('id') === 'Packages'){
+            $ob = Rays::find(request()->session()->get('userId'));
+            request()->validate(['id' => ['required', Rule::in(isset($ob[request()->route('id')]) ? array_keys($ob[request()->route('id')]) : array())],
+            ], [
+                'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')]['PackagesIdRequired'],
+                'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')]['PackagesIdInvalid'],
+            ]);
+            $this->getDeleteDatabade($ob, request()->route('id'));
+            $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')]['PackagesDelete'];
+        }else if(request()->route('id') === 'Cultures'){
+            $ob = Rays::find(request()->session()->get('userId'));
+            request()->validate(['id' => ['required', Rule::in(isset($ob[request()->route('id')]) ? array_keys($ob[request()->route('id')]) : array())],
+            ], [
+                'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')]['CulturesIdRequired'],
+                'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')]['CulturesIdInvalid'],
+            ]);
+            $this->getDeleteDatabade($ob, request()->route('id'));
+            $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')]['CulturesDelete'];
+        }else if(request()->route('id') === 'Contracts'){
+            $ob = Rays::find(request()->session()->get('userId'));
+            request()->validate(['id' => ['required', Rule::in(isset($ob[request()->route('id')]) ? array_keys($ob[request()->route('id')]) : array())],
+            ], [
+                'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')]['ContractIdRequired'],
+                'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')]['ContractIdInvalid'],
+            ]);
+            $this->getDeleteDatabade($ob, request()->route('id'));
+            $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')]['ContractsDelete'];
+        }else if(request()->route('id') === 'Knows'){
+            $ob = Rays::find(request()->session()->get('userId'));
+            request()->validate(['id' => ['required', Rule::in(isset($ob[request()->route('id')]) ? array_keys($ob[request()->route('id')]) : array())],
+            ], [
+                'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')]['KnowsIdRequired'],
+                'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')]['KnowsIdInvalid'],
+            ]);
+            $this->getDeleteDatabade($ob, request()->route('id'));
+            $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')]['KnowsDelete'];
+        }else if(request()->route('id') === 'Patent'){
+            $ob = Rays::find(request()->session()->get('userId'));
+            request()->validate(['id' => ['required', Rule::in(isset($ob[request()->route('id')]) ? array_keys($ob[request()->route('id')]) : array())],
+            ], [
+                'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')]['PatentIdRequired'],
+                'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')]['PatentIdInvalid'],
+            ]);
+            $this->getDeleteDatabade($ob, request()->route('id'));
+            $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')]['PatientsDelete'];
+        }else //if(request()->route('id') === 'Receipt')
+        {
+            $ob = Rays::find(request()->session()->get('userId'));
+            request()->validate(['id' => ['required', Rule::in(isset($ob[request()->route('id')]) ? array_keys($ob[request()->route('id')]) : array())],
+            ], [
+                'id.required'=>$ob[$ob['Setting']['Language']][request()->route('id')]['PatientRegisterationIdRequired'],
+                'id.in'=>$ob[$ob['Setting']['Language']][request()->route('id')]['PatientRegisterationIdInvalid'],
+            ]);
+            $this->getDeleteDatabade($ob, request()->route('id'));
+            $this->successfully1 = $ob[$ob['Setting']['Language']][request()->route('id')]['PatientRegisterationDelete'];
         }
     }
     public function action($id){
