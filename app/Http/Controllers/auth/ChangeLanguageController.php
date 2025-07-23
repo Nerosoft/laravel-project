21678265923070e7a8a5748b3560a44cc0d6c693
unknown <ggamals7744@yahoo.com>
2025-07-23 19:42:40 +0300

    edit code

--- app/Http/Controllers/auth/ChangeLanguageController.php
+++ app/Http/Controllers/auth/ChangeLanguageController.php
@@ -12,22 +12,18 @@ use Illuminate\Support\Facades\Validator;
 
 
 use App\language\share\Page;
-use App\Http\interface\ActionInit2;
 use App\Http\interface\ValidRull;
 
-class ChangeLanguageController extends Page implements ActionInit2, ValidRull
+class ChangeLanguageController extends Page implements ValidRull
 {
     public function __construct(){
         $this->ob = Rays::find(request()->session()->get('userId'))?Rays::find(request()->session()->get('userId')):(Rays::find(request()->input('userAdmin'))?Rays::find(request()->input('userAdmin')):Rays::first());
         parent::__construct($this, Route::currentRouteName() === 'branchMain'?'Branch':'ChangeLanguage', $this->ob);
-    }
-    public function initValid(){
         array_push($this->roll['id'], Route::currentRouteName() === 'branchMain' || Route::currentRouteName() === 'branch.delete'?
         Rule::notIn(request()->session()->get('userId')):Rule::notIn(request()->session()->get('userId')?$this->ob['Setting']['Language']:(request()->cookie(request()->input('userAdmin'))&&request()->input('id') === unserialize(request()->cookie(request()->input('userAdmin')))?unserialize(request()->cookie(request()->input('userAdmin'))):array())));
         $this->message['not_in'] = $this->ob[$this->ob['Setting']['Language']][Route::currentRouteName() === 'branchMain'?'Branch':'ChangeLanguage']['IdIsInv'];
     }
     public function initValidRull(){
-        $this->initValid();
         return Route::currentRouteName() === 'branch.delete'?Rule::in(Rays::find(request()->session()->get('userLogout'))['Branch']?array_keys(Rays::find(request()->session()->get('userLogout'))['Branch']):null):Rule::in(Route::currentRouteName() === 'branchMain'?(Rays::find(request()->session()->get('userLogout'))['Branch']?array_merge([request()->session()->get('userLogout')],array_keys(Rays::find(request()->session()->get('userLogout'))['Branch'])):request()->session()->get('userLogout')):array_keys($this->ob[$this->ob['Setting']['Language']]['AllNamesLanguage']));
     }
     public function makeChangeBranch(){   
